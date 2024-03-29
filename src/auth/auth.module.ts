6997b77921727554ddf2136fe487873ec468b858
Grey Boolean <greyboolean@gmail.com>
2024-03-29 19:23:28 +0530

    create jwt-config.module to avoid circular dependency in users.module and auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,22 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtConfigModule } from 'src/jwt-config/jwt-config.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    ConfigModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: configService.get('JWT_EXPIRES_IN') },
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [UsersModule, JwtConfigModule],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { JwtConfigModule } from 'src/jwt-config/jwt-config.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), JwtConfigModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
